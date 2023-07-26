203d28d7c8ea983edbf2db4ea8a1428485ef9c30
Cody Kochmann <kochmanncody@gmail.com>
2023-07-26 09:02:49 -0600

    Created generate-pdf.sh to automate future conversions

---
+++ generate-pdf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# this file runs the code needed to convert README.md to resume.pdf
+
+set -euxo pipefail
+
+export TARGET='README.md'
+export OUTPUT_FILE='resume.pdf'
+
+test -f "$TARGET"
+
+pandoc "$TARGET" --wrap=preserve -f markdown -o "$OUTPUT_FILE"
+
+test -f "$OUTPUT_FILE"
